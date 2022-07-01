8371879f490d125b6383525f28333503039981af
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-07-01 07:14:28 +0430

    Definition of class Buy in buy.h

--- includes/buy.h
+++ includes/buy.h
@@ -1,4 +1,42 @@
 #ifndef BUY_H
 #define BUY_H
 
+#include "Date.h"
+#include "good.h"
+
+class Buy
+{
+public:
+    void setSellerName(string);
+    string getSellerName() const;
+    void setProductID(unsigned int);
+    unsigned int getProductID() const;
+    void setAmount(float);
+    float getAmount() const;
+    void setNumber(int);
+    int getNumber() const;
+    void setPurchaseDateS(string);
+    string getPurchaseDateS() const;
+    void setExpirationDate(string);
+    string getExpirationDate() const;
+    void saveBuyData() const;
+    void setPurchaseDateYear(int);
+    void setPurchaseDateMonth(int);
+    void setPurchaseDateDay(int);
+    void setPurchaseDateHour(int);
+    void setPurchaseDateMinute(int);
+    void setExpirationDateYear(int);
+    void setExpirationDateMonth(int);
+    void setExpirationDateDay(int);
+    void setExpirationDateHour(int);
+    void setExpirationDateMinute(int);
+    Date getPurchaseDate() const;
+private:
+    string sellerName;
+    unsigned int productID;
+    float amount;
+    int number;
+    Date purchaseDate, expirationDate;
+};
+
 #endif
