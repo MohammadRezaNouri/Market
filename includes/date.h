0105f0fb44c9f5401b184bd7813b5d890d5151ce
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-07-01 07:12:32 +0430

    Definition of class Date in date.h

--- includes/date.h
+++ includes/date.h
@@ -1,4 +1,29 @@
 #ifndef DATE_H
 #define DATE_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Date
+{
+public:
+    void setYear(int);
+    int getYear() const;
+    void setMonth(int);
+    int getMonth() const;
+    void setDay(int);
+    int getDay() const;
+    void setHour(int);
+    int getHour() const;
+    void setMinute(int);
+    int getMinute() const;
+    void setDate(string);
+    string getDate() const;
+
+private:
+    int day, month, year, hour, minute;
+};
+
 #endif
