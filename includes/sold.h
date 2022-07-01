05ace35da25d60ecea06d0d554ed1e89b39ff96b
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-07-01 07:15:00 +0430

    Definition of class Sold in sold.h

--- includes/sold.h
+++ includes/sold.h
@@ -1,4 +1,41 @@
 #ifndef SOLD_H
 #define SOLD_H
 
+#include "Date.h"
+#include "good.h"
+
+#include <vector>
+
+class good;
+
+class Sold
+{
+public:
+    unsigned int getCustomerCode() const;
+    void setProductID(unsigned int, int, good[], vector<Sold>, unsigned int);
+    unsigned int getProductID() const;
+    void setAmount(float);
+    float getAmount() const;
+    void setNumber(int);
+    int getNumber() const;
+    void setAmountDiscount(float);
+    float getAmountDiscount() const;
+    void setSaleDate(string);
+    string getSaleDate() const;
+    void saveSoldData() const;
+    void setSaleDateYear(int);
+    void setSaleDateMonth(int);
+    void setSaleDateDay(int);
+    void setSaleDateHour(int);
+    void setSaleDateMinute(int);
+    Date getDate();
+
+private:
+    unsigned int customerCode, productID;
+    float amount, amountDiscount;
+    int number;
+    Date saleDate;
+    void setCustomerCode(int, good[], vector<Sold>);
+};
+
 #endif
